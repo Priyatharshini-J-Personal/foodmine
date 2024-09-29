e56a485ce893e86a020096c03b11152f9ea68f7f
priyatharshini.ja <priyatharshini.ja@zohocorp.com>
2024-09-30 00:17:52 +0530

    frontend completed

---
+++ frontend/src/app/components/partials/not-found/not-found.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css'
+})
+export class NotFoundComponent {
+  
+@Input()
+visible = false;
+
+@Input()
+notFoundMessage = "Nothing found!!!"
+
+
+@Input()
+resetLinkText = "Reset"
+
+@Input()
+resetLinkRoute = "/"
+
+constructor(){}
+}
