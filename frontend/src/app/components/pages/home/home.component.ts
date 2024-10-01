e1071c3acfa75377fa92705795061aabca94ab52
priyatharshini.ja <priyatharshini.ja@zohocorp.com>
2024-10-01 10:31:56 +0530

    backend completed

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/Food';
 import { FoodService } from '../../../services/food.service';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +12,18 @@ import { ActivatedRoute } from '@angular/router';
 export class HomeComponent {
  foods:Food[] = []
  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute){
+  let foodsObservalbe:Observable<Food[]>;
   activatedRoute.params.subscribe((params) => {
     if(params.searchTerm)
-      this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm)
+      foodsObservalbe = this.foodService.getAllFoodsBySearchTerm(params.searchTerm)
     else if (params.tag)
-      this.foods = this.foodService.getAllFoodsByTag(params.tag)
+      foodsObservalbe = this.foodService.getAllFoodsByTag(params.tag)
     else
-      this.foods = foodService.getAll()
+      foodsObservalbe = foodService.getAll()
+
+      foodsObservalbe.subscribe((serverFoods) => {
+        this.foods = serverFoods;
+      })
   })
  }
 }
